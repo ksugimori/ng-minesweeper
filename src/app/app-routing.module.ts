8ba70d9866e7ef9653992f3cf70d9399a03a2f9b
ksugimori <ksugimori.private@gmail.com>
2021-09-18 23:19:34 +0900

    ページコンポーネント作成

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MainPageComponent } from './main-page/main-page.component';
+import { PlayPageComponent } from './play-page/play-page.component';
+import { SettingPageComponent } from './setting-page/setting-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: MainPageComponent },
+  { path: 'play', component: PlayPageComponent },
+  { path: 'settings', component: SettingPageComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainPageComponent } from './main-page/main-page.component';
+import { SettingPageComponent } from './setting-page/setting-page.component';
+import { PlayPageComponent } from './play-page/play-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainPageComponent,
+    SettingPageComponent,
+    PlayPageComponent
   ],
   imports: [
     BrowserModule,
