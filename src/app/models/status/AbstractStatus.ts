5e7a464befe6597485efd8ccdbc358f117136fa8
ksugimori <ksugimori.private@gmail.com>
2021-09-20 22:28:39 +0900

    抽象クラスを abstract を使って書き換え

--- src/app/models/status/AbstractStatus.ts
+++ src/app/models/status/AbstractStatus.ts
@@ -4,35 +4,34 @@ import { Point } from "../util/Point";
 /**
  * ゲームの状態
  */
-export class AbstractStatus {
+export abstract class AbstractStatus {
+  /** 名称 */
   public name: string;
 
-  constructor (name: string) {
+  /**
+   * コンストラクタ
+   * @param name 名称
+   */
+  constructor(name: string) {
     this.name = name
   }
 
   /**
    * 終了状態か？
    */
-  get isEnd (): boolean {
-    throw new Error(`Method Unimplemented! isEnd()`)
-  }
+  abstract get isEnd(): boolean
 
   /**
    * 指定した座標のセルを開く。
    * @param game ゲーム
    * @param point 座標
    */
-  open (game: Game, point: Point) {
-    throw new Error(`Method Unimplemented! open(${game}, ${point})`)
-  }
+  abstract open(game: Game, point: Point): void
 
   /**
    * 指定した座標のセルにフラグを立てる。
    * @param game ゲーム
    * @param point 座標
    */
-  flag (game: Game, point: Point) {
-    throw new Error(`Method Unimplemented! flag(${game}, ${point}`)
-  }
+  abstract flag(game: Game, point: Point): void
 }
